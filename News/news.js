d0fac39b0945dd4994e3e84002b879ae4f03d4cc
Your Name <you@example.com>
2021-11-12 09:26:01 +0800

    全部網站完成

--- News/news.js
+++ News/news.js
@@ -51,7 +51,7 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
