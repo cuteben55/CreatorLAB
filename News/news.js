2f8bb1329b70c9f406bd2ec8957a088e156544a6
Your Name <you@example.com>
2021-11-16 10:43:18 +0800

    story完成

--- News/news.js
+++ News/news.js
@@ -65,3 +65,21 @@ function topFunction() {
 }
 
 
+// 導覽列縮合
+$(function(){
+  
+  // 點擊按鈕，選單縮放
+  $("button.btn_switch").on("click", function(){
+    $("nav").slideToggle();
+  });
+  
+});
+
+$(function(){
+  
+  // hamburger icon 的切換
+  $("button.hamburger").on("click", function(){
+    $(this).toggleClass("is-active");
+  });
+
+});
